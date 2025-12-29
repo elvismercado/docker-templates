0f967fea2c8f33c1653aa596cbc9accca95ef052
JuveScript <github@juvescript.com>
2025-12-29 21:16:35 +0100

    Update

--- media/managers/utilities/striptracks/striptracks-custom.sh
+++ media/managers/utilities/striptracks/striptracks-custom.sh
@@ -10,4 +10,16 @@
 
 # If running manualy use -f for filename
 
-. /usr/local/bin/striptracks.sh ---audio :org:eng:spa:nld:dut:jpn:chi:zho:und --subs :org:eng:spa:nld:dut --reorder
+# . /usr/local/bin/striptracks.sh --audio :org:eng:spa:nld:dut:jpn:chi:zho:und --subs :org:eng:spa:nld:dut --reorder
+
+# keep the list of audio languages
+# keep the list of subtitle tracks
+# reorder audio and subtitles
+# set the default subtitles track to the first non-forced English track
+# . /usr/local/bin/striptracks.sh --audio :org:eng:spa:nld:dut:jpn:chi:zho:und --subs :org:eng:spa:nld:dut:jpn:chi:zho --reorder --set-default-audio :org --set-default-subs :eng-f
+. /usr/local/bin/striptracks.sh \
+    --audio :org:eng:spa:nld:jpn:zho:und \
+    --subs  :org:eng:spa:nld:jpn:zho \
+    --reorder \
+    --set-default-audio :org \
+    --set-default-subs :eng-f
